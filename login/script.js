665e15863939817521c3e7977052ff51d34edf6e
dev-alexandre2024 <alexandrecdsdev@gmail.com>
2025-07-04 11:45:39 -0300

    alterando e corrigindo as funçoes do javascript

--- login/script.js
+++ login/script.js
@@ -5,18 +5,18 @@ function validarformulario(){
 
 
     let resultado = "";
-    if (idade < 18){
-        resultado +=`você tem apenas ${idade} anos voce e menor de idade`;
-    }
-    else if(idade > 18){
-         resultado +=`você tem ${idade} você é maior de idade`;
-    }
+
+
 
      resultado += `<br>Nome ${nome}`;
      resultado += `<br>Cidade ${cidade}`;
     
      var h1nome = document.getElementById('text');
-     h1nome.textContent = `Olá ${nome.value}, você tem ${idade} anos, e mora em ${cidade}.`;  
+     h1nome.textContent = `Olá ${nome.value} , Você Tem ${idade} anos, e Mora Em ${cidade}`  ;
 
-     document.getElementById('resultado').innerHTML = resultado;
+
+     
+     
+
+     document.getElementById('resultado').innerHTML.resultado;
 }
